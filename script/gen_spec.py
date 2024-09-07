43b43a924c2b0d584bc1ffa5fd60b571f6226a5a
wanghenshui <wanghenshui@qq.com>
2024-09-07 23:22:23 +0800

    fix #129

---
+++ script/gen_spec.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+#!/usr/bin/python3
+import time
+def main():
+    title = input("新文章名字 不要有空格:")
+
+
+    all = ""
+    with open("./spec/template.md", "r+",errors="ignore") as f:
+        data = f.read()
+        all = data.replace('NNN', str(val))
+        all = all.replace("%Y-%m-%d", time.strftime("%Y-%m-%d"))
+
+    filename_prefix = "./spec/"
+
+    filename = "%Y-%m-%d-", time.strftime("%Y-%m-%d")+filename_prefix + ".md"
+
+    with open(filename, "w") as f:
+        f.write(all)
+
+if __name__ == "__main__":
+    
+    main()
---
+++ spec/template.md
@@ -0,0 +1,25 @@
+---
+layout: post
+title:  title
+---
+# NNN title
+
+`TODO add README then remove this line`
+
+[周刊项目地址](https://github.com/wanghenshui/cppweeklynews)
+
+公众号
+
+<img src="https://wanghenshui.github.io/cppweeklynews/assets/code.png" alt=""  width="30%">
+
+点击「查看原文」跳转到 GitHub 上对应文件，链接就可以点击了
+
+qq群 [点击进入](https://qm.qq.com/q/6NGizNPyG4)
+
+[RSS](https://github.com/wanghenshui/cppweeklynews/releases.atom)
+
+欢迎投稿，推荐或自荐文章/软件/资源等，评论区留言
+
+本期文章由 特约供稿，感谢@作者
+
+---
